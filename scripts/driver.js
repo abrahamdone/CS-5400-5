d9c6cdb3c18b2cb7a31fa4ab5203c7029c8f6671
Abraham Done <andone@mac.com>
2025-06-09 08:35:49 -0600

    copy from previous assignment

---
+++ scripts/driver.js
@@ -0,0 +1,129 @@
+
+// noinspection JSVoidFunctionReturnValueUsed
+
+MySample.main = (function() {
+    'use strict';
+
+    const canvas = document.getElementById('canvas-main');
+    const gl = canvas.getContext('webgl2');
+
+    let vertices = {};
+    let indices = {};
+    let vertexColors = {};
+
+    let shaderProgram = {};
+    let indexBuffer = {};
+
+    //------------------------------------------------------------------
+    //
+    // Scene updates go here.
+    //
+    //------------------------------------------------------------------
+    function update() {
+    }
+
+    //------------------------------------------------------------------
+    //
+    // Rendering code goes here
+    //
+    //------------------------------------------------------------------
+    function render() {
+        gl.clearColor(
+            0.3921568627450980392156862745098,
+            0.58431372549019607843137254901961,
+            0.92941176470588235294117647058824,
+            1.0);
+        gl.clearDepth(1.0);
+        gl.depthFunc(gl.LEQUAL);
+        gl.enable(gl.DEPTH_TEST);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+    }
+
+    //------------------------------------------------------------------
+    //
+    // This is the animation loop.
+    //
+    //------------------------------------------------------------------
+    function animationLoop(time) {
+
+        update();
+        render();
+
+        requestAnimationFrame(animationLoop);
+    }
+
+    async function initialize() {
+        console.log('initializing...');
+
+        const vertexShaderSource = await loadFileFromServer('assets/shaders/simple.vert');
+        const fragmentShaderSource = await loadFileFromServer('assets/shaders/simple.frag');
+        initializeShaders(vertexShaderSource, fragmentShaderSource);
+
+        initializeVertices();
+        initializeBufferObjects();
+
+        requestAnimationFrame(animationLoop);
+    }
+    function initializeVertices() {
+        vertices = new Float32Array([
+            0.0, 0.5, 0.0,
+            0.5, -0.5, 0.0,
+            -0.5, -0.5, 0.0
+        ]);
+        indices = new Uint16Array([ 0, 1, 2 ]);
+        vertexColors = new Float32Array([
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0
+        ]);
+    }
+    function initializeBufferObjects() {
+        let vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        indexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+        let vertexColorBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        let position = gl.getAttribLocation(shaderProgram, 'aPosition');
+        gl.enableVertexAttribArray(position);
+        gl.vertexAttribPointer(position, 3, gl.FLOAT, false, vertices.BYTES_PER_ELEMENT * 3, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+        let color = gl.getAttribLocation(shaderProgram, 'aColor');
+        gl.enableVertexAttribArray(color);
+        gl.vertexAttribPointer(color, 3, gl.FLOAT, false, vertexColors.BYTES_PER_ELEMENT * 3, 0);
+    }
+
+    function initializeShaders(vertexShaderSource, fragmentShaderSource) {
+        let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertexShader, vertexShaderSource);
+        gl.compileShader(vertexShader);
+        console.log(gl.getShaderInfoLog(vertexShader)); // for debugging
+
+        let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragmentShader, fragmentShaderSource);
+        gl.compileShader(fragmentShader);
+        console.log(gl.getShaderInfoLog(fragmentShader));
+
+        shaderProgram = gl.createProgram();
+        gl.attachShader(shaderProgram, vertexShader);
+        gl.attachShader(shaderProgram, fragmentShader);
+        gl.linkProgram(shaderProgram);
+        gl.useProgram(shaderProgram);
+    }
+
+    initialize();
+
+}());
